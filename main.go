b860eec8047ae4ff35dcec6ab72531655cfab62e
even <even03060@163.com>
2022-03-27 22:21:53 +0800

    添加版本号

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"mysql_backup_go/controller"
 	"os"
 )
 
 func main() {
-	logfile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	log.SetOutput(logfile)
-	if err != nil {
-		log.Panicf("打开日志文件失败：%v", err)
-	}
-	c := controller.NewController("config.json", "dbs.txt")
-	err = c.Controller()
-	if err != nil {
-		log.Panic(err)
+	for _, args := range os.Args[1:] {
+		if args == "" {
+			logfile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+			log.SetOutput(logfile)
+			if err != nil {
+				log.Panicf("打开日志文件失败：%v", err)
+			}
+			c := controller.NewController("config.json", "dbs.txt")
+			err = c.Controller()
+			if err != nil {
+				log.Panic(err)
+			}
+		} else {
+			if args == "-v" || args == "--version" {
+				fmt.Printf("mysql_backup\nversion:0.9\n")
+			} else {
+				fmt.Printf("参数错误，可以使用以下参数：\n[-v|--version]\n")
+			}
+		}
 	}
 }
