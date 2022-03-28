746ad55c6bc4b0e61501565a7b238cb4af896e4d
even <even03060@163.com>
2022-03-28 23:36:43 +0800

    更换版本号实现方式

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"mysql_backup_go/controller"
 	"os"
@@ -9,24 +9,21 @@ import (
 
 func main() {
 	version := "0.9.2"
-	for _, args := range os.Args {
-		if len(os.Args) < 2 {
-			logfile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-			log.SetOutput(logfile)
-			if err != nil {
-				log.Panicf("打开日志文件失败：%v", err)
-			}
-			c := controller.NewController("config.json", "dbs.txt")
-			err = c.Controller()
-			if err != nil {
-				log.Panic(err)
-			}
-		} else {
-			if args == "-v" || args == "--version" {
-				fmt.Printf("mysql_backup\n" + version + "\n")
-			} else {
-				fmt.Printf("参数错误，可以使用以下参数：\n[-v|--version]\n")
-			}
-		}
+	printVersion := flag.Bool("version", false, "[--version]")
+	flag.Parse()
+	if *printVersion {
+		println(version)
+		os.Exit(0)
+	}
+
+	logfile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	log.SetOutput(logfile)
+	if err != nil {
+		log.Panicf("打开日志文件失败：%v", err)
+	}
+	c := controller.NewController("config.json", "dbs.txt")
+	err = c.Controller()
+	if err != nil {
+		log.Panic(err)
 	}
 }
