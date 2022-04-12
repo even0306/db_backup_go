f5b4799062b1604fe35fd32f648390cd05ee7c8e
even <even03060@163.com>
2022-04-12 18:53:23 +0800

    修复相关bug，遗留postgresql指定库备份失败问题

--- modules/databaseComparison.go
+++ modules/databaseComparison.go
@@ -1,11 +1,7 @@
 package modules
 
 import (
-	"bufio"
-	"bytes"
 	"db_backup_go/common"
-	"errors"
-	"fmt"
 )
 
 type Comparison interface {
@@ -26,8 +22,8 @@ func NewCompartor(conf *common.ConfigFile, dbs *[]string) *comparisonInfo {
 
 func (c *comparisonInfo) Comparison() (*[]string, error) {
 	//获取所有数据库名
-	var out *[]byte
 	var err error
+	var allDbs *[]string
 	dbi := DBInfo{
 		DBHost:     c.conf.DB_HOST,
 		DBPort:     c.conf.DB_PORT,
@@ -36,46 +32,29 @@ func (c *comparisonInfo) Comparison() (*[]string, error) {
 	}
 	dbu := NewDBDumpFunc(c.conf.MYSQL_EXEC_PATH, &dbi)
 	if c.conf.DATABASETYPE == "mysql" {
-		out, err = dbu.GetMysqlDBList()
+		allDbs, err = dbu.GetMysqlDBList()
 		if err != nil {
 			return nil, err
 		}
 	} else if c.conf.DATABASETYPE == "postgresql" {
-		out, err = dbu.GetPostgresqlDBList()
+		allDbs, err = dbu.GetPostgresqlDBList()
 		if err != nil {
 			return nil, err
 		}
-	}
-
-	var allDbs []string
-
-	rf := bytes.NewReader(*out)
-	bf := bufio.NewScanner(rf)
-
-	for {
-		flag := bf.Scan()
-		line := bf.Text()
-		if err != nil && errors.Is(err, bf.Err()) == true {
-			return nil, fmt.Errorf("逐行读取系统数据库名失败：%w", err)
-		}
-		if flag == false {
-			break
-		}
-		allDbs = append(allDbs, line)
 	}
 
 	//根据筛选方式，筛选出待备份的数据库
 	var preDBS []string
 	var flag = false
 	if c.conf.FILTER_METHOD == true {
-		for _, v := range allDbs {
+		for _, v := range *allDbs {
 			for _, w := range *c.dbs {
 				if w == "all" {
 					preDBS = nil
 					preDBS = append(preDBS, "all")
 					flag = true
 					break
-				} else if w == string(v) {
+				} else if " "+w == string(v) {
 					preDBS = append(preDBS, w)
 				}
 			}
@@ -84,7 +63,7 @@ func (c *comparisonInfo) Comparison() (*[]string, error) {
 			}
 		}
 	} else {
-		for _, v := range allDbs {
+		for _, v := range *allDbs {
 			for _, w := range *c.dbs {
 				if w == "all" {
 					preDBS = nil
--- modules/dbdump.go
+++ modules/dbdump.go
@@ -2,19 +2,19 @@ package modules
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
-
-	_ "github.com/bmizerany/pq"
+	"strings"
 )
 
 type DBDump interface {
 	MysqlDump(db *string) ([]byte, error)
 	MysqlDumpAll() ([]byte, error)
-	GetMysqlDBList() (*[]byte, error)
+	GetMysqlDBList() (*[]string, error)
 	PostgresqlDump(db *string) ([]byte, error)
 	PostgresqlDumpAll() ([]byte, error)
-	GetPostgresqlDBList() (*[]byte, error)
+	GetPostgresqlDBList() (*[]string, error)
 }
 
 type dbDump struct {
@@ -62,18 +62,19 @@ func (d *dbDump) MysqlDumpAll() (*[]byte, error) {
 }
 
 // 使用mysql客户端查看mysql数据库现有的库，返回[]byte的数据库列表
-func (d *dbDump) GetMysqlDBList() (*[]byte, error) {
+func (d *dbDump) GetMysqlDBList() (*[]string, error) {
 	cmd := exec.Command(d.dumpExecPath+"/mysql", "-h"+d.DBHost, "-P"+strconv.Itoa(d.DBPort), "-u"+d.DBUser, "-p"+d.DBPassword, "-Bse", "show databases")
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("stderr: %w", err)
 	}
-	return &out, nil
+	list := strings.Split(string(out), "\n")
+	return &list, nil
 }
 
 // 使用pg_dump备份postgresql数据库，传入DBInfo结构体和要备份的数据库名指针，返回备份出的[]byte数据指针和错误
 func (d *dbDump) PostgresqlDump(db *string) (*[]byte, error) {
-	cmd := exec.Command(d.dumpExecPath+"/pg_dump", "\"host="+d.DBHost, "port="+strconv.Itoa(d.DBPort), "user="+d.DBUser, "password="+d.DBPassword+"\"", "-t", *db, "--inserts")
+	cmd := exec.Command(d.dumpExecPath+"/pg_dump", "-h", d.DBHost, "-p", strconv.Itoa(d.DBPort), "-U", d.DBUser, "-d", *db, "--inserts")
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf(*db+"数据库配置失败：%w", err)
@@ -83,7 +84,27 @@ func (d *dbDump) PostgresqlDump(db *string) (*[]byte, error) {
 
 // 使用pg_dump备份postgresql数据库，传入DBInfo结构体，返回备份出的[]byte数据指针和错误
 func (d *dbDump) PostgresqlDumpAll() (*[]byte, error) {
-	cmd := exec.Command(d.dumpExecPath+"/pg_dumpall", "\"host="+d.DBHost, "port="+strconv.Itoa(d.DBPort), "user="+d.DBUser, "password="+d.DBPassword+"\"", "--inserts")
+	cmd := exec.Command(d.dumpExecPath+"/pg_dumpall", "-h", d.DBHost, "-p", strconv.Itoa(d.DBPort), "-U", d.DBUser, "--inserts")
+
+	env := os.Environ()
+	cmdEnv := []string{}
+	flag := false
+	for _, e := range env {
+		i := strings.Index(e, "=")
+		if i > 0 && (e[:i] == "PGPASSWORD") {
+			e = "PGPASSWORD=" + d.DBPassword
+			cmdEnv = append(cmdEnv, e)
+			flag = true
+			break
+		} else {
+			cmdEnv = append(cmdEnv, e)
+		}
+	}
+	if flag == false {
+		cmdEnv = append(cmdEnv, "PGPASSWORD="+d.DBPassword)
+	}
+	cmd.Env = cmdEnv
+
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("all数据库配置失败：%w", err)
@@ -91,19 +112,13 @@ func (d *dbDump) PostgresqlDumpAll() (*[]byte, error) {
 	return &out, nil
 }
 
-func (d *dbDump) GetPostgresqlDBList() (*[]byte, error) {
-	// cmd1 := exec.Command(d.dumpExecPath+"/psql", fmt.Sprintf("host=%s port=%v user=%s password=%s", d.DBHost, d.DBPort, d.DBUser, d.DBPassword), "-c", "SELECT datname FROM pg_database;")
-	cmd1 := exec.Command(d.dumpExecPath+"/psql", fmt.Sprintf("host=%s port=%v user=%s password=%s", d.DBHost, d.DBPort, d.DBUser, d.DBPassword), "-c", fmt.Sprint("SELECT datname FROM pg_database;"))
-	cmd2 := exec.Command("awk", fmt.Sprint("{if (NR>2){print $1}}"))
-	cmd3 := exec.Command("awk", fmt.Sprint("NR>1 {print last} {last=$0}"))
-	cmd4 := exec.Command("awk", fmt.Sprint("NR>1 {print last} {last=$0}"))
-	cmd2.Stdin, _ = cmd1.StdoutPipe()
-	cmd3.Stdin, _ = cmd2.StdoutPipe()
-	cmd4.Stdin, _ = cmd3.StdoutPipe()
-	out, err := cmd4.Output()
+func (d *dbDump) GetPostgresqlDBList() (*[]string, error) {
+	cmd := exec.Command(d.dumpExecPath+"/psql", fmt.Sprintf("host=%s port=%v user=%s password=%s", d.DBHost, d.DBPort, d.DBUser, d.DBPassword), "-c", fmt.Sprint("SELECT datname FROM pg_database;"))
+	out, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("stderr: %w", err)
 	}
-	fmt.Println(string(out))
-	return &out, nil
+	list := strings.Split(string(out), "\n")
+	list = list[2 : len(list)-3]
+	return &list, nil
 }
